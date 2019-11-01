f116a37b475bf36935e049924277fb4d57757f53
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-31 21:46:48 -0600

    Wrap similar data in divs for styling and add apiCalls test file

--- src/Containers/BeerPage/BeerPage.js
+++ src/Containers/BeerPage/BeerPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './BeerPage.scss'
+import './BeerPage.scss';
 
 export const BeerPage = ({ beerDetails, match }) => {
   const allHops = beerDetails.ingredients.hops.map((hop, index) => {
@@ -44,53 +44,68 @@ export const BeerPage = ({ beerDetails, match }) => {
 
   return (
     <div className='BeerPage' key={beerDetails.id}>
-      <section className='BeerPage__section--header'>
-      <h2 className="BeerPage__h2--beerName">{beerDetails.name}</h2>
-      <h3>{beerDetails.tagline}</h3>
+      <section className='BeerPage__section--left'>
+        <section className='BeerPage__section--stats'>
+          <ul>
+            <li>ABV: {beerDetails.abv}</li>
+            <li>Attenuation Level: {beerDetails.attenuation_level}</li>
+            <li>
+              Boil volume: {beerDetails.boil_volume.value}{' '}
+              {beerDetails.boil_volume.unit}
+            </li>
+            <li>EBC: {beerDetails.ebc}</li>
+            <li>IBU: {beerDetails.ibu}</li>
+            <li>PH: {beerDetails.ph}</li>
+            <li>SRM: {beerDetails.srm}</li>
+            <li>Target FG: {beerDetails.target_fg}</li>
+            <li>Target OG: {beerDetails.target_og}</li>
+          </ul>
+        </section>
+        <aside className='BeerPage__aside--ingredients'>
+          <h4>Ingredients</h4>
+          <ul>
+            <h3>Hops</h3>
+            {allHops}
+          </ul>
+          <ul>
+            <h3>Malts</h3>
+            {allMalts}
+          </ul>
+          <p>{beerDetails.ingredients.yeast}</p>
+        </aside>
       </section>
-      <h5>Description</h5>
-      <p>{beerDetails.description}</p>
-      <h5>Brewer's Tips</h5>
-      <p>{beerDetails.brewers_tips}</p>
-      <ul>{allFoods}</ul>
-      <h5>
-        Total volume this makes: {beerDetails.volume.value}{' '}
-        {beerDetails.volume.unit}
-      </h5>
-      <h5>Temperature</h5>
-      <ul>{mash_temp}</ul>
-      <h5>
-        Fermentation: {beerDetails.method.fermentation.temp.value}{' '}
-        {beerDetails.method.fermentation.temp.unit}
-      </h5>
-      <section className="BeerPage__section--stats">
-      <ul>
-        <li>ABV: {beerDetails.abv}</li>
-        <li>Attenuation Level: {beerDetails.attenuation_level}</li>
-        <li>
-          Boil volume: {beerDetails.boil_volume.value}{' '}
-          {beerDetails.boil_volume.unit}
-        </li>
-        <li>EBC: {beerDetails.ebc}</li>
-        <li>IBU: {beerDetails.ibu}</li>
-        <li>PH: {beerDetails.ph}</li>
-        <li>SRM: {beerDetails.srm}</li>
-        <li>Target FG: {beerDetails.target_fg}</li>
-        <li>Target OG: {beerDetails.target_og}</li>
-      </ul>
+      <section className='BeerPage__section--right'>
+        <section className='BeerPage__section--header'>
+          <h2 className='BeerPage__h2--beerName'>{beerDetails.name}</h2>
+          <h3>{beerDetails.tagline}</h3>
+        </section>
+        <section>
+          <div className='BeerPage__div--description'>
+            <h5>Description</h5>
+            <p>{beerDetails.description}</p>
+          </div>
+          <div className='BeerPage__div--brewersTips'>
+            <h5>Brewer's Tips</h5>
+            <p>{beerDetails.brewers_tips}</p>
+          </div>
+          <div className='BeerPage__div--pairsWith'>
+            <h4>Pairs well with:</h4>
+            <ul>{allFoods}</ul>
+          </div>
+        </section>
+        <section>
+          <h5>
+            Total volume this makes: {beerDetails.volume.value}{' '}
+            {beerDetails.volume.unit}
+          </h5>
+          <h5>Mash temperature</h5>
+          <ul>{mash_temp}</ul>
+          <h5>
+            Fermentation: {beerDetails.method.fermentation.temp.value}{' '}
+            {beerDetails.method.fermentation.temp.unit}
+          </h5>
+        </section>
       </section>
-      <aside className="BeerPage__aside--ingredients">
-        <h4>Ingredients</h4>
-        <ul>
-          Hops
-          {allHops}
-        </ul>
-        <ul>
-          Malts
-          {allMalts}
-        </ul>
-        <p>{beerDetails.ingredients.yeast}</p>
-      </aside>
     </div>
   );
 };
