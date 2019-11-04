9387cd0b09192c8cef9eb54c70679922ae4533c3
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-03 23:45:23 -0700

    Add reducer testing coverage for bookmarks

--- src/reducers/beers.test.js
+++ src/reducers/beers.test.js
@@ -31,25 +31,3 @@ describe('beers', () => {
     expect(beers(initialState, incorrectAction)).toEqual(initialState);
   });
 });
-
-// it('should output the correct case of IS_LOADING action type', () => {
-//   const initialState = '';
-//   const action = {
-//     type: 'IS_LOADING',
-//     bool: false
-//   };
-
-//   const result = false
-
-//   expect(setLoading(initialState, action)).toEqual(result);
-// })
-
-// it('should not output the case of IS_LOADING action type', () => {
-//   const initialState = '';
-//   const wrongAction = {
-//     type: 'WRONG_ACTION',
-//     movies: [{}, {}, {}]
-//   };
-
-//   expect(setLoading(initialState, wrongAction)).toEqual(initialState);
-// })
---
+++ src/reducers/bookmarks.test.js
@@ -0,0 +1,55 @@
+import { bookmarks } from './bookmarks';
+
+describe('bookmarks', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+
+    const result = bookmarks(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should output the correct case of SET_BOOKMARK action type', () => {
+    const initialState = [1];
+    const action = {
+      type: 'SET_BOOKMARK',
+      beer: 2
+    };
+
+    const result = [1, 2];
+
+    expect(bookmarks(initialState, action)).toEqual(result);
+  });
+
+  it('should not output the case of SET_BOOKMARK action type', () => {
+    const initialState = [];
+    const incorrectAction = {
+      type: 'DELETE_BOOKMARK',
+      beer: 2
+    };
+
+    expect(bookmarks(initialState, incorrectAction)).toEqual(initialState);
+  });
+
+  it('should output the correct case of REMOVE_BOOKMARK action type', () => {
+    const initialState = [1, 2];
+    const action = {
+      type: 'REMOVE_BOOKMARK',
+      beer: 1
+    };
+
+    const result = [2];
+
+    expect(bookmarks(initialState, action)).toEqual(result);
+  });
+
+  it('should not output the case of REMOVE_BOOKMARK action type with the incorrect action', () => {
+    const initialState = [1, 2];
+    const wrongAction = {
+      type: 'WRONG_ACTION',
+      movies: [{}, {}, {}]
+    };
+
+    expect(bookmarks(initialState, wrongAction)).toEqual(initialState);
+  });
+});
