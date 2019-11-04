375247cc92273607ee9aea76e352df96d9135cdb
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-04 08:36:59 -0700

    Add testing coverage for SearchForm Container Component

--- src/Containers/SearchForm/SearchForm.test.js
+++ src/Containers/SearchForm/SearchForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { setSearchResults } from '../../actions';
 import { SearchForm, mapStateToProps, mapDispatchToProps } from './SearchForm';
 
 describe('SearchForm', () => {
@@ -27,6 +28,12 @@ describe('SearchForm', () => {
       }
     }
   ];
+  const mockEvent = {
+    target: {
+      name: 'searchTerm',
+      value: 'Stout'
+    }
+  };
 
   beforeEach(() => {
     wrapper = shallow(<SearchForm beers={beers} />);
@@ -34,5 +41,64 @@ describe('SearchForm', () => {
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should update it's state property onChange of searchTerm input", () => {
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state()).toEqual({
+      searchTerm: 'Stout',
+      hops: '',
+      malt: '',
+      yeast: ''
+    });
+  });
+
+  it('should call handleClick on submit', () => {
+    wrapper.instance().handleSubmit = jest.fn();
+    wrapper.instance().forceUpdate();
+    wrapper.find('.SearchForm').simulate('submit');
+
+    expect(wrapper.instance().handleSubmit).toHaveBeenCalled();
+  });
+
+  it('should call handleClick within handleSubmit', () => {
+    const event = { preventDefault: () => {} };
+    jest.spyOn(event, 'preventDefault');
+    wrapper.instance().handleClick = jest.fn();
+    wrapper.instance().handleSubmit(event);
+
+    expect(event.preventDefault).toBeCalled();
+    expect(wrapper.instance().handleClick).toHaveBeenCalled();
+  });
+});
+
+describe('mapStateToProps', () => {
+  it('should return an object with a beers array and searchResults array', () => {
+    const mockStoreState = {
+      beers: [{}, {}, {}],
+      searchResults: [{}]
+    };
+
+    const expected = {
+      beers: [{}, {}, {}],
+      searchResults: [{}]
+    };
+
+    const mappedProps = mapStateToProps(mockStoreState);
+
+    expect(mappedProps).toEqual(expected);
+  });
+});
+
+describe('mapDispatchToProps', () => {
+  it('calls dispatch with a setSearchResults, when handleClick is called', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = setSearchResults([{}, {}, {}]);
+
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.setSearchResults([{}, {}, {}]);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   });
 });
